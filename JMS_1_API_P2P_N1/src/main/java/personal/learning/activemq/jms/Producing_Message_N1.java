6663bda4847f79c8c51121fb09ff5636d2cc6499
Aman Mishra <mishraamankpa@gmail.com>
2024-03-31 01:06:23 +0530

    JMS 1 API point to point messaging

---
+++ JMS_1_API_P2P_N1/src/main/java/personal/learning/activemq/jms/Producing_Message_N1.java
@@ -0,0 +1,54 @@
+package personal.learning.activemq.jms;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+/*
+ * Run Producer first
+ */
+public class Producing_Message_N1 {
+	public static void main(String[] args) {
+		ConnectionFactory connectionFactoty = new ActiveMQConnectionFactory("tcp://localhost:61616");
+		Connection connection = null;
+		Session session = null;
+		try {
+			connection = connectionFactoty.createConnection();
+			session = connection.createSession();
+
+			Queue queue = session.createQueue("myQueue");
+
+			MessageProducer producer = session.createProducer(queue);
+
+			String message = "Hello! My name is Aman";
+			TextMessage textMessage = session.createTextMessage(message);
+
+			producer.send(textMessage);
+			System.out.println("Message sent: " + message);
+
+		} catch (JMSException e) {
+			e.printStackTrace();
+		} finally {
+
+			try {
+				if (session != null) {
+					session.close();
+				}
+
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+}
---
+++ JMS_1_API_PubSub_N5/src/main/java/personal/learning/activemq/jms/Producing_Message_N1.java
@@ -0,0 +1,53 @@
+package personal.learning.activemq.jms;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+/*
+ * Producer must be running second because we have created non-durableSubscription
+ */
+public class Producing_Message_N1 {
+	
+	public static void main(String[] args) {
+		
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+		
+		Connection connection = null;
+		Session session = null;
+		
+		try {
+			connection = connectionFactory.createConnection();
+			session = connection.createSession();
+			
+			Topic topic = session.createTopic("topic2");
+			
+			MessageProducer producer = session.createProducer(topic);
+			
+			String msg = "How are you Aman ?";
+			TextMessage textMessage = session.createTextMessage(msg);
+			producer.send(textMessage);
+			System.out.println("Message sent: " + textMessage.getText());
+			
+		} catch(JMSException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(session != null) {
+					session.close();
+				}
+				
+				if(connection != null) {
+					connection.close();
+				}
+			} catch(JMSException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
