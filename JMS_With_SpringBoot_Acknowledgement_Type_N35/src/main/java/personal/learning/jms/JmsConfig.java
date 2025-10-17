b0204c3190f8f4a020543f4a86d61bbad7332858
Aman Mishra <mishraamankpa@gmail.com>
2025-10-17 13:58:12 +0530

    PubSub messaging

--- JMS_With_SpringBoot_Acknowledgement_Type_N35/src/main/java/personal/learning/jms/JmsConfig.java
+++ JMS_With_SpringBoot_Acknowledgement_Type_N35/src/main/java/personal/learning/jms/JmsConfig.java
@@ -13,6 +13,33 @@ import org.springframework.jms.listener.DefaultMessageListenerContainer;
 import personal.learning.jms.receiver.DemoMessageReceiver;
 import personal.learning.jms.receiver.DummyMessageReceiver;
 
+/*
+ 
+1. AUTO_ACKNOWLEDGE : 1
+ 
+Behavior: JMS automatically acknowledges the message after the listener method returns successfully.
+Implication: No redelivery in case of listener exception or JVM crash during processing. 
+
+2. DUPS_OK_ACKNOWLEDGE : 3
+
+Behavior: Acknowledges lazily, allowing potential duplicate deliveries.
+Use case: When throughput is prioritized over strict delivery guarantees. 
+
+3. CLIENT_ACKNOWLEDGE : 2
+
+Behavior: Client code explicitly calls message.acknowledge() to confirm receipt.
+Implication: Gives you control to delay acknowledgment until after business logic executes. Acknowledging one message 
+acknowledges all messages consumed so far in that session.
+
+4. SESSION_TRANSACTED (Transaction Mode) : 0
+
+Behavior: Treats message receipt and processing as a transaction.
+Action: Calling session.commit() (done automatically) acknowledges the message only on commit.
+Guarantees: Ensures no message loss or duplication — if processing fails, rollback triggers redelivery.
+ 
+*/
+
+
 @Configuration
 public class JmsConfig {
 	
